a92030aa706e4045676561688199582da0b8ea56
Finic <finic@mail.ru>
2023-02-02 22:44:59 +0300

    Fix: 8 time

--- popup.js
+++ popup.js
@@ -1,8 +1,14 @@
 const openPopup = document.getElementById('open_popup');
+const closePopup = document.getElementById('close_popup')
 const savePopup = document.getElementById('save_popup');
-const mainPopup = document.getElementsByClassName('popup');
+const mainPopup = document.getElementById('main_popup');
 
 openPopup.addEventListener('click', function(e){
     e.preventDefault();
-    mainPopup.classList('active');
+    mainPopup.classList.add('active');
+})
+
+closePopup.addEventListener('click', function(e){
+    e.preventDefault();
+    mainPopup.classList.remove('active');
 })
