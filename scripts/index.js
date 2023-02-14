eda18c3259d3206ebae423372b262f02254021b8
Finic <finic@mail.ru>
2023-02-15 02:13:12 +0300

    fix: 24 time

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,56 @@
+const mainPopup = document.querySelector('#main_popup');
 const openPopup = document.querySelector('.profile__edit-button');
-const openCard = document.querySelector('.profile__add-button')
-const closePopup = document.querySelector('.popup__close-button')
-const savePopup = document.querySelector('.popup__save-button');
-const mainPopup = document.querySelector('.popup');
-const cardPopup = document.querySelector('#card-popup');
-let editNameProfile = document.querySelector('.profile__name');
-let editDestinyProfile = document.querySelector('.profile__destiny');
-let editPlaceCard = document.querySelector('.card__title');
-let editImageCard = document.querySelector('.card__image');
-let nameInPopup = document.querySelector('.popup__input.popup__input_type_name');
-let destinyInPopup = document.querySelector('.popup__input.popup__input_type_destiny');
-let placeInPopupAdd = document.querySelector('.popup__input.popup__input_type_placename');
-let imageInPopupAdd = document.querySelector('.popup__input.popup__input_type_imagename');
+const closePopup = document.querySelector('#profil-close_popup');
+const savePopup = document.querySelector('#profile-save_popup');
+const editNameProfile = document.querySelector('.profile__name');
+const editDestinyProfile = document.querySelector('.profile__destiny');
+const nameInPopup = document.querySelector('.popup__input_type_name');
+const destinyInPopup = document.querySelector('.popup__input_type_destiny');
 
+const cardPopup = document.querySelector('#card-popup');
+const openCard = document.querySelector('.profile__add-button');
+const closeCard = document.querySelector('#card-close_popup');
+const savePopupCard = document.querySelector('#card-save_popup');
+const placeInPopupCard = document.querySelector('.popup__input_type_placename');
+const imageInPopupCard= document.querySelector('.popup__input_type_imagename');
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardAddList = document.querySelector('.elements__list');
+
+const initialCards = [
+    {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+    
+
+initialCards.forEach((item) => {
+    const cardElements = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElements.querySelector('.card__image').src = item.link
+    cardElements.querySelector('.card__title').textContent = item.name
+    cardAddList.append(cardElements);
+});
 
 function profileEdit(e){
     e.preventDefault();
@@ -25,22 +63,24 @@ function popupClose(){
     mainPopup.classList.remove('popup_opened');
 }
 
+function popupCloseCard(){
+    cardPopup.classList.remove('popup_opened');
+}
+
 function popupOpen(){
     mainPopup.classList.add('popup_opened');
     nameInPopup.value = editNameProfile.textContent;
     destinyInPopup.value = editDestinyProfile.textContent;
 }
 
-function popupOpenAdd(){
+function popupOpenCard(){
     cardPopup.classList.add('popup_opened');
-    placeInPopupAdd.value = editPlaceCard.textContent;
-    imageInPopupAdd.value = editImageCard.textContent;
 }
 
 openPopup.addEventListener('click', popupOpen);
-
 closePopup.addEventListener('click', popupClose);
-
 mainPopup.addEventListener('submit', profileEdit);
 
-cardPopup.addEventListener('click', popupOpenAdd)
+
+openCard.addEventListener('click', popupOpenCard);
+closeCard.addEventListener('click', popupCloseCard);
